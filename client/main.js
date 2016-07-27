466a8adf7d37662d24e5285c3b7a2b710c1482f4
MrEraba <ivan_almanza_@hotmail.com>
2016-07-27 03:19:53 -0600

    add the following actions for Products collection: update, delete, and search

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 import { Template } from 'meteor/templating';
 import {Products} from '../lib/api/products.js';
+import {Session} from 'meteor/session';
+import {ReactiveVar} from 'meteor/reactive-var';
 import './main.html';
 
 Template.headerTemplate.onRendered(function () {
@@ -71,6 +73,56 @@ Template.headerTemplate.events({
     },
 });
 
+Template.inventoryTemplate.onRendered(function () {
+    $('.collapsible').collapsible({
+        accordion : true
+    });
+});
+
+Template.inventoryTemplate.onCreated(function () {
+    Meteor.subscribe('products');
+    this.searchQuery = new ReactiveVar( '' );
+});
+
+Template.inventoryTemplate.helpers({
+    'products'(){
+        if (Template.instance().searchQuery.get() !== ''){
+            return Products.find({name: {'$regex': Template.instance().searchQuery.get()}});
+        } else {
+            return Products.find({});
+        }
+    }
+});
+
+Template.inventoryTemplate.events({
+    'keyup #search-query'(){
+        if ($('#search-query').val() === ''){
+            Template.instance().searchQuery.set('');
+        } else {
+            Template.instance().searchQuery.set($('#search-query').val());
+        }
+    }
+});
+
+Template.productTemplate.helpers({
+    'isAvilable'(){
+        return this.available;
+    }
+});
+
+Template.productTemplate.events({
+    'click #js-delete-product'(){
+        Meteor.call('products.remove', this._id);
+    },
+    'click #js-available-product'(){
+        Meteor.call('products.setAvailable', this._id, this.available)
+    },
+    'click #js-edit-product'(){
+        Session.set("productForEdit", this._id);
+        FlowRouter.go('/admin/product/edit');
+    }
+});
+
 Template.newProductForm.events({
     'submit #js-new-product-form'(event){
         event.preventDefault();
@@ -98,22 +150,36 @@ Template.newProductForm.events({
         target["product_description"].value = '';
         target["product_image"].value = '';
 
-        FlowRouter.go('/admin/inventory');
+        //FlowRouter.go('/admin/products');
     }
 });
 
-Template.inventoryTemplate.onRendered(function () {
-    $('.collapsible').collapsible({
-        accordion : true
-    });
-});
-
-Template.inventoryTemplate.onCreated(function () {
+Template.editProductForm.onCreated(function () {
     Meteor.subscribe('products');
 });
 
-Template.inventoryTemplate.helpers({
-    'products'(){
-        return Products.find({});
+Template.editProductForm.helpers({
+    'product'(){
+        return Products.findOne({_id: Session.get("productForEdit")});
     }
 });
+
+Template.editProductForm.events({
+    'submit #js-edit-product-form' (event){
+        event.preventDefault();
+        const target = event.target;
+
+        const product = {
+            name: target["product_name"].value,
+            code: target["product_code"].value,
+            image: target["product_image"].value,
+            amount: target["product_amount"].value,
+            description: target["product_description"].value,
+            price: target["product_price"].value
+        };
+        Meteor.call('products.update', Session.get("productForEdit") ,product);
+        Session.set("productForEdit", "");
+        FlowRouter.go('/admin/products');
+    }
+});
+
